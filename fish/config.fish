e1b94bf529c3e1e5966dd93d4fd795b4681abd13
jonjonjonjonjonjonjonjonjonjonjonjon <joaogilbertotomasbarreto@gmail.com>
2026-05-07 21:54:14 +0100

    added fastfetch

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    fastfetch
 end
 set fish_greeting ""
 oh-my-posh init fish --config $HOME/.config/OhMyPosh/half-life.omp.json | source
