b4448ac652edba86c9950797f65f339dfdf90830
jonjonjonjonjonjonjonjonjonjonjonjon <joaogilbertotomasbarreto@gmail.com>
2026-05-09 16:39:28 +0100

    vim binds in fish

--- fish/config.fish
+++ fish/config.fish
@@ -3,4 +3,5 @@ if status is-interactive
     toilet Jλ-Term -f mono12 --termwidth
 end
 set fish_greeting ""
+set -g fish_key_bindings fish_vi_key_bindings
 oh-my-posh init fish --config $HOME/.config/OhMyPosh/half-life.omp.json | source
