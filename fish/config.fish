681ef6f42ee99dcb68b71579c990aeb34c0f03a4
jonjonjonjonjonjonjonjonjonjonjonjon <joaogilbertotomasbarreto@gmail.com>
2026-05-09 16:21:26 +0100

    added cool terminal ascii

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    fastfetch
+    toilet Jλ-Term -f mono12 --termwidth
 end
 set fish_greeting ""
 oh-my-posh init fish --config $HOME/.config/OhMyPosh/half-life.omp.json | source
-fish_add_path /home/hp/.clispot/bin
-
-export SPOTIFY_CLIENT_ID="dc3f4e38f41644e29b2666a0bda91d6f"
-export SPOTIFY_CLIENT_SECRET="58b508b343064eefa4ebe67d24e62450"
-export BEMOJI_TYPE_CMD="wtype"
